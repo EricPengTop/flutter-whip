55afac9ec72327fe87c9b670de88c887b3d8dc0a
pengtao <pengtao@elementech.net>
2024-12-30 16:24:49 +0800

    -modify

--- lib/src/transports/http.dart
+++ lib/src/transports/http.dart
@@ -5,26 +5,32 @@ import 'package:http/http.dart' as http;
 import 'package:http/io_client.dart';
 
 late IOClient ioClient;
-bool initliazed = false;
+bool initialized = false;
 
 void initHttpClient() {
-  if (!initliazed) {
+  if (!initialized) {
     var trustSelfSigned = true;
     var httpClient = HttpClient()
       ..badCertificateCallback =
           ((X509Certificate cert, String host, int port) => trustSelfSigned);
     ioClient = IOClient(httpClient);
-    initliazed = true;
+    initialized = true;
   }
 }
 
-// POST/PATCH/DELETE
-Future<http.Response> httpPost(Uri url,
-        {Map<String, String>? headers, Object? body, Encoding? encoding}) =>
+Future<http.Response> httpPost(
+  Uri url, {
+  Map<String, String>? headers,
+  Object? body,
+  Encoding? encoding,
+}) =>
     ioClient.post(url, headers: headers, body: body, encoding: encoding);
 
-Future<http.Response> httpPatch(Uri url,
-    {Map<String, String>? headers, Object? body}) async {
+Future<http.Response> httpPatch(
+  Uri url, {
+  Map<String, String>? headers,
+  Object? body,
+}) async {
   final response = await ioClient.patch(url, headers: headers, body: body);
   print('Status code: ${response.statusCode}');
   print('Body: ${response.body}');
